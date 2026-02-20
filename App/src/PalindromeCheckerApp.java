ce3663fae74ec36b7372001270bb31ea9fe4a82d
VigunhSaini <sainivigunh0312@gmail.com>
2026-02-20 09:17:10 +0530

    VigunhSaini added UseCase1

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,26 @@
+//Version 1.0
+//Author: Vigunh
+//UseCase1: Welcome Page
+public class PalindromeCheckerApp {
+
+    // Application constants
+    private static final String APP_NAME = "Palindrome Checker App";
+    private static final String VERSION = "Version 1.0";
+
+    // Entry point of the application
+    public static void main(String[] args) {
+
+        // Welcome Message
+        System.out.println("====================================");
+        System.out.println(" Welcome to " + APP_NAME);
+        System.out.println(" " + VERSION);
+        System.out.println("====================================");
+
+        // Application Flow Control
+        System.out.println("Application started successfully.");
+        System.out.println("Ready to validate palindromes...");
+
+        // Next Use Case can be invoked here
+        // Example: startPalindromeValidation();
+    }
+}
